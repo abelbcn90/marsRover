dc88ae8ff64b291232e37f8d9c97a4f1c77f7dd8
Abel <Abel@Abel-PC.home>
2017-09-09 13:36:49 +0200

    Create project

---
+++ marsRover/src/main/java/kata/marsRover/App.java
@@ -0,0 +1,21 @@
+package kata.marsRover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private int xCoordinate, yCoordinate;
+	private String face;
+	private final static String SPIN_90_DEGREES_LEFT = "L";
+	private final static String SPIN_90_DEGREES_RIGHT = "R";
+	private final static String MOVE= "M";
+	
+	public static void main(String[] args) {
+		
+		// (x, y) is (x, y + 1)
+		
+		System.out.println("Hello World!");
+	}
+}
